1123b464d781af145087021df716214d74ae3f78
ricorodriguez6596-boop <ricorodriguez6596@gmail.com>
2026-06-29 09:31:01 +0530

    change

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+let icon=document.querySelector('.icon')
+let headerLink=document.querySelector('.header-links')
+let nav=document.querySelector('nav')
+
+
+let change=()=>{
+    headerLink.classList.toggle('display')
+}
+
+
+icon.addEventListener('click', change)
+icon.addEventListener('click',add)
